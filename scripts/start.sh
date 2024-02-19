9fef1183f3ecc96eb9c7b42ae3e2f36e129d797e
101dal <101dalcontact@gmail.com>
2024-02-19 07:51:42 +0100

    Changed start.sh to only restart the server if a new version has been found

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,21 +9,22 @@ LOG_DIR="./logs"
 # Every how many seconds it should check the new release
 CHECK_TIME=86400
 
+(cd $ASSETS_DIR && bun run "server.js" | ccze -m ansi | tee -a "../$LOG_FILE" 2>&1 &)
 while true; do
-
-  # Stop server
-  kill $(pgrep -f bun)
 
   # Restart timer
   START_TIME=$(date +%Y%m%d%H%M%S)
 
-  # Create new log file
-  LOG_FILE="$LOG_DIR/server_$START_TIME.log"
-
-  touch "$LOG_FILE"
-
   # Check for new release every $CHECK_TIME seconds
   if [ $(($(date +%s) - $START_TIME)) -ge $CHECK_TIME ]; then
+
+    # Create new log file
+    LOG_FILE="$LOG_DIR/server_$START_TIME.log"
+
+    touch "$LOG_FILE"
+  
+    # Stop server
+    kill $(pgrep -f bun)
 
     # Get latest tag
     TAG=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
@@ -48,11 +49,11 @@ while true; do
     cp $ASSETS_DIR/backup/.env $ASSETS_DIR/
     cp -r $ASSETS_DIR/backup/assets $ASSETS_DIR/
 
-  fi
+    
+    # Start server
+    (cd $ASSETS_DIR && bun run "server.js" | ccze -m ansi | tee -a "../$LOG_FILE" 2>&1 &)
 
-  # Start server
-  echo $LOG_FILE
-  (cd $ASSETS_DIR && bun run "server.js" | ccze -m ansi | tee -a "../$LOG_FILE" 2>&1 &)
+  fi
 
   # Loop
   sleep $CHECK_TIME
